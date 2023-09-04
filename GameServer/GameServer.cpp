55207d51c08c085ec7bc9f309fae070e91d78850
c01d5t3w_l3n0v0 <coldstew@gmail.com>
2023-09-05 02:37:36 +0900

    Thread Create Test

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,7 +2,37 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread()
+{
+	cout <<  "Hello Thread" << endl;
+}
+
+void HelloThread_2(int32 num)
+{
+	cout << num << endl;
+}
+
 int main()
 {
-	HelloWorld();
+	std::thread t;
+	
+	//int32 count = t.hardware_concurrency();
+	//auto id = t.get_id();
+
+	vector<std::thread> v;
+	
+	for (int32 i = 0; i < 10; i++)
+	{
+		v.push_back(std::thread(HelloThread_2, i));
+	}
+
+	for (int32 i = 0; i < 10; i++)
+	{
+		if (v[i].joinable())
+			v[i].join();
+	}
+
+	cout << "Hello Main" << endl;
 }
